c9e28d644df8fdb7a7a948f833973f86b2384e0c
Vani <vaninarwani@gmail.com>
2025-05-22 14:29:19 +0530

    react

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <h2>
+    <h2 >
         Expense Tracker
       
     </h2>
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -17,8 +17,26 @@ export const GlobalContext=createContext(initialState);
 //provider component
 export const GlobalProvider = ({children}) => {
     const [state,dispatch] = useReducer(AppReducer,initialState);
+
+    function deleteTransaction(id){
+      dispatch({
+        type:'DELETE_TRANSACTION',
+        payload:id
+      });
+    }
+    function addTransactions(transaction){
+      dispatch({
+        type:'ADD_TRANSACTION',
+        payload:transaction
+      });
+    }
+
+
+
     return(<GlobalContext.Provider value={{
-        transactions:state.transactions
+        transactions:state.transactions,
+        deleteTransaction,
+        addTransactions
     }}>
       {children}
     </GlobalContext.Provider>);
